37244cc493a9d67748262d7e8ddfab54c3d5a953
Rigweasle <parada.rigo@gmail.com>
2022-06-25 04:42:44 -0700

    Initial Commit

---
+++ src/exceptionExample/Phone.java
@@ -0,0 +1,30 @@
+package exceptionExample;
+
+public class Phone {
+    String phoneType;
+    String phoneNumber;
+
+    public Phone(String phoneType,String phoneNumber) {
+        if (phoneType == null || phoneNumber == null) {
+            throw new IllegalArgumentException("The type and number cannot be null");
+        }
+        this.phoneType = phoneType;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPhoneType() {
+        return phoneType;
+    }
+    public void setPhoneType(String phoneType) {
+        this.phoneType = phoneType;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    @Override
+    public String toString() {
+        return "Phone Type: " + phoneType + " Phone Number: " + phoneNumber;
+    }
+}
